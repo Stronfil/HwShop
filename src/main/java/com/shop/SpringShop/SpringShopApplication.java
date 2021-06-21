fa26eb32ec0c03279952ba33667355c4e89ebf77
Stronfil <tambovskii99@gmail.com>
2021-06-21 16:49:25 +0300

    Initial commit

---
+++ src/main/java/com/shop/SpringShop/SpringShopApplication.java
@@ -0,0 +1,18 @@
+package com.shop.SpringShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class SpringShopApplication {
+
+	public static void main(String[] args) {
+		run(SpringShopApplication.class, args);
+	}
+
+}
+
+
+
